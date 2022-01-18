112bb2e214089895c27af918fa1ff88d5cc822d5
Mayla <mjsilva@vertigo.com.br>
2022-01-17 23:29:03 -0300

    Apagando Opcoes.class e adicionando arraylist

--- src/TestaSistema.java
+++ src/TestaSistema.java
@@ -1,10 +1,17 @@
+import java.util.ArrayList;
+
 public class TestaSistema {
 
 	public static void main(String[] args) throws Exception {
 
-		Opcoes opcoes = new Opcoes();
+		ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
 
-		opcoes.menuDeOpcoes();
+		Usuario usuario1 = new Usuario();
+		usuarios.add(usuario1);
+		usuario1.menuDeOpcoes();
 
+		Usuario usuario2 = new Usuario();
+		usuarios.add(usuario2);
+		usuario2.menuDeOpcoes();
 	}
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 
@@ -11,6 +13,7 @@ public class Usuario {
 
 	Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 	String data = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(timestamp.getTime());
+	BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
 
 	public void cadastrar(String nome, String cpf) {
 		this.nome = nome;
@@ -22,17 +25,20 @@ public class Usuario {
 	public void visualizarRotas() {
 
 		System.out.println(
-				"A - 3 conexÃµes, tempo total de voo 3 horas\n" + "B - Voo direto, tempo total de voo 6 horas\n");
+				"A - 3 conexões, tempo total de voo 3 horas\n" + "B - Voo direto, tempo total de voo 6 horas\n");
 	}
 
-	public void reservarRota(char rota) {
+	public void reservarRota(char rota) throws Exception {
 
-		if (cadastrado) {
+		System.out.println("Digite seu CPF para confirmar:");
+		String cpf = entrada.readLine();
+
+		if (this.cpf.equals(cpf)) {
 			this.rota = Character.toUpperCase(rota);
 			reservarPassagem(this.rota);
 			this.rotaReservada = true;
 		} else {
-			System.out.println("Precisa estar cadastrado");
+			System.out.println("CPF incorreto.");
 		}
 	}
 
@@ -41,10 +47,8 @@ public class Usuario {
 		if (rota == 'A' || rota == 'B') {
 			System.out.println(data + " Passagem reservada por " + this.nome + " Rota " + this.rota + " escolhida");
 			this.rota = rota;
-		} else if (rota != 'A' || rota != 'B') {
-			System.out.println("Entrada Iválida");
 		} else {
-			System.out.println("Precisa estar cadastrado");
+			System.out.println("Entrada Inválida");
 		}
 	}
 
@@ -59,4 +63,59 @@ public class Usuario {
 
 	}
 
+	public void menuDeOpcoes() throws Exception {
+		System.out.println("...................................");
+		System.out.println("Selecione o que deseja fazer:\n" + "1 - Cadastro de Usuários \n"
+				+ "2 - Reserva de Passagens \n" + "3 - Cancelamento de Reservas \n" + "4 - Sair");
+		System.out.println("...................................");
+
+		int opcao = Integer.parseInt(entrada.readLine());
+
+		selecionar(opcao);
+	}
+
+	public void selecionar(int opcao) throws Exception {
+		switch (opcao) {
+		case 1:
+			System.out.println("Por favor insira seu nome:");
+			String nome = entrada.readLine();
+			System.out.println("Agora, insira seu CPF:");
+			String cpf = entrada.readLine();
+			cadastrar(nome, cpf);
+			menuDeOpcoes();
+			break;
+		case 2:
+			if (cadastrado) {
+				System.out.println("Por favor, selecione a rota para que possamos liberar a sua passagem.\n");
+				visualizarRotas();
+				System.out.println("Selecione uma das rotas disponiveis.");
+				char rota = entrada.readLine().charAt(0);
+				reservarRota(rota);
+				menuDeOpcoes();
+			} else {
+				System.out.println("Precisa estar cadastrado!");
+				menuDeOpcoes();
+			}
+			break;
+		case 3:
+			if (cadastrado) {
+				cancelarReserva();
+			} else {
+				System.out.println("Precisa estar cadastrado!");
+			}
+			menuDeOpcoes();
+			break;
+		case 4:
+			if (this.nome != null) {
+				System.out.println("O Usuario " + this.nome + " saiu...");
+			} else {
+				System.out.println("Usuario não identificado saiu...");
+			}
+			break;
+		default:
+			System.out.println("Opção Inválida");
+			menuDeOpcoes();
+			break;
+		}
+	}
 }
